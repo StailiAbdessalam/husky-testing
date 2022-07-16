16828500fe877b0998685c36b6dc9c83fffdd515
BENJY33X <A.STAILI@student.youcode.ma>
2022-07-16 17:09:00 +0100

    test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn Husky/i);
+  const linkElement = screen.getByText(/learn REACT/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App ">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Husky
+          Learn REACT
         </a>
       </header>
     </div>
